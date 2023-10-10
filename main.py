134c69d6e7e3a02d9c75cfe9ac4d9178e9e23c1d
kmqasim055 <135268580+kmqasim055@users.noreply.github.com>
2023-10-11 02:24:59 +0500

    main.py

--- main.py
+++ main.py
@@ -6,13 +6,14 @@
 
 import argparse
 from bank_parser.bank_of_america import parse_bank_of_america
+from bank_parser.capital_one_bank import parse_capital_one_bank
 
 def main():
     # Set up the argument parser
     parser = argparse.ArgumentParser(description='Process bank statements.')
 
     # Add an argument for the bank name
-    parser.add_argument('bank', type=str, help='Name of the bank to process')
+    parser.add_argument('bank', nargs='?', type=str, help='Name of the bank to process')
 
     # Parse the command-line arguments
     args = parser.parse_args()
@@ -20,21 +21,13 @@ def main():
     # Now, you can access the bank name using args.bank
     bank = args.bank
     
-    if bank == "Bank of America":
+    if bank == "Bank of America" or bank is None:
         parse_bank_of_america()
-    elif bank == "PNC Bank":
+    if bank == "Capital One Bank" or bank is None:
+        parse_capital_one_bank()
+    if bank == "PNC Bank" or bank is None:
         pass
-    else:
-        # Run parse functions of all banks
-        parse_bank_of_america()    
 
 
 if __name__ == "__main__":
     main()
-
-
-# In[ ]:
-
-
-
-
