5ac2318e573abca5296cd4ed6febafad7a1ccee0
Muhammad Qasim Khan <135268580+kmqasim055@users.noreply.github.com>
2024-01-18 04:55:48 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
 import argparse
 from bank_parser.bank_of_america import parse_bank_of_america
 from bank_parser.capital_one_bank import parse_capital_one_bank
 from bank_parser.CITIBANK import parse_CITIBANK
 from bank_parser.Redacted_Bank import parse_redacted_bank
+from bank_parser.PNC_Bank import parse_pnc_bank
 
 def main():
     # Set up the argument parser
@@ -30,13 +25,12 @@ def main():
     if bank == "CITIBANK" or bank is None:
         parse_CITIBANK()
     if bank == "Redacted_Bank" or bank is None:
-	parse_redacted_bank()
+        parse_redacted_bank()
+    if bank == "PNC_Bank" or bank is None:
+        parse_pnc_bank()
     if bank == "PNC Bank" or bank is None:
         pass
 
 
 if __name__ == "__main__":
     main()
-
-
-# In[ ]:
